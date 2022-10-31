3f9b0f670e5287a42cab7a19642d30c168dc683d
ranguli <hello@joshmurphy.ca>
2022-10-31 13:29:58 -0230

    Fix quoting

--- {{cookiecutter.plugin_name}}/noxfile.py
+++ {{cookiecutter.plugin_name}}/noxfile.py
@@ -12,8 +12,8 @@ def test(session):
 @session(python=["3.9"])
 def lint(session):
     session.run("black", ".", external=True)
-    session.run("flake8", "./" + {{cookiecutter.module_name}}, "./test", external=True)
-    session.run("bandit", "-r", "./" + {{cookiecutter.module_name}}, external=True)
-    session.run("mypy", "--warn-unreachable", "./" + {cookiecutter.module_name}}, external=True)
-    session.run("pylint", "./" + {{cookiecutter.module_name}}, external=True)
+    session.run("flake8", "./{{cookiecutter.module_name}}", "./test", external=True)
+    session.run("bandit", "-r", "./{{cookiecutter.module_name}}", external=True)
+    session.run("mypy", "--warn-unreachable", "./{{cookiecutter.module_name}}", external=True)
+    session.run("pylint", "./{{cookiecutter.module_name}}", external=True)
     session.run("isort", ".", external=True)
